e52076255bfcb0adf49b7bbb7d15c8b800755bba
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-11-19 15:32:26 +0100

    added new solution to codewars

--- codewars.js
+++ codewars.js
@@ -111,25 +111,36 @@
 // }
 
 // console.log(accum("ZpglnRxqenU"));
-function likes(names) {
-  let whoLikesIt = [];
+// function likes(names) {
+//   let whoLikesIt = [];
 
-  if (names.length === 0) {
-    return "no one likes this";
-  } else if (names.length === 1) {
-    return `${names[0]} likes this`;
-  } else if (names.length === 2) {
-    return `${names[0]} and ${names[1]} like this`;
-  } else if (names.length === 3) {
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  } else if (names.length > 3) {
-    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//   if (names.length === 0) {
+//     return "no one likes this";
+//   } else if (names.length === 1) {
+//     return `${names[0]} likes this`;
+//   } else if (names.length === 2) {
+//     return `${names[0]} and ${names[1]} like this`;
+//   } else if (names.length === 3) {
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//   } else if (names.length > 3) {
+//     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//   }
+// }
+
+// console.log(likes([]));
+// console.log(likes(["Peter"]));
+// console.log(likes(["Jacob", "Alex"]));
+// console.log(likes(["Max", "John", "Mark"]));
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+// console.log(likes(["Alex", "Jacob", "Mark", "Max", "Dawid"]));
+function isPrime(num) {
+  if (num <= 0) {
+    return false;
+  } else if (num > 0 && num / num === 1 && num % 2 !== 0) {
+    return true;
+  } else {
+    return console.log("anything");
   }
 }
 
-console.log(likes([]));
-console.log(likes(["Peter"]));
-console.log(likes(["Jacob", "Alex"]));
-console.log(likes(["Max", "John", "Mark"]));
-console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
-console.log(likes(["Alex", "Jacob", "Mark", "Max", "Dawid"]));
+console.log(isPrime(-1));
