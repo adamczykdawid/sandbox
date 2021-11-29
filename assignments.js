5488c3b7f34bed9e2a1f9fdc543df7e9121f1cd1
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-11-29 23:10:19 +0100

    added new assignment file'

--- assignments.js
+++ assignments.js
@@ -276,47 +276,56 @@
 // }
 // console.log(percentages, percentages3);
 
-const newDeck = () => {
-  return {
-    deck: [],
-    drawnCards: [],
-    suits: ["hearts", "spades", "clubs", "diamonds"],
-    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
-    initializeDeck() {
-      const { deck, values, suits } = this;
-      const newValues = values.split(",");
-      for (let suit of suits) {
-        for (let value of newValues) {
-          const onePair = { suit, value };
-          deck.push(onePair);
-        }
-      }
-      return deck;
-    },
-    drawCard() {
-      const card = this.deck.pop();
-      this.drawnCards.push(card);
-      return card;
-    },
-    drawMultiple(value) {
-      const cards = [];
-      for (let i = 1; i <= value; i++) {
-        cards.push(this.drawCard());
-      }
-      return cardsj;
-    },
-    shuffle() {
-      let currentIdx = this.deck.length,
-        randomIdx;
-      while (currentIdx != 0) {
-        randomIdx = Math.floor(Math.random() * currentIdx);
-        currentIdx--;
-        [this.deck[currentIdx], this.deck[randomIdx]] = [
-          this.deck[randomIdx],
-          this.deck[currentIdx],
-        ];
-      }
-      return this.deck;
-    },
-  };
+// const newDeck = () => {
+//   return {
+//     deck: [],
+//     drawnCards: [],
+//     suits: ["hearts", "spades", "clubs", "diamonds"],
+//     values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+//     initializeDeck() {
+//       const { deck, values, suits } = this;
+//       const newValues = values.split(",");
+//       for (let suit of suits) {
+//         for (let value of newValues) {
+//           const onePair = { suit, value };
+//           deck.push(onePair);
+//         }
+//       }
+//       return deck;
+//     },
+//     drawCard() {
+//       const card = this.deck.pop();
+//       this.drawnCards.push(card);
+//       return card;
+//     },
+//     drawMultiple(value) {
+//       const cards = [];
+//       for (let i = 1; i <= value; i++) {
+//         cards.push(this.drawCard());
+//       }
+//       return cardsj;
+//     },
+//     shuffle() {
+//       let currentIdx = this.deck.length,
+//         randomIdx;
+//       while (currentIdx != 0) {
+//         randomIdx = Math.floor(Math.random() * currentIdx);
+//         currentIdx--;
+//         [this.deck[currentIdx], this.deck[randomIdx]] = [
+//           this.deck[randomIdx],
+//           this.deck[currentIdx],
+//         ];
+//       }
+//       return this.deck;
+//     },
+//   };
+// };
+//coding challange jonas
+const printForecast = (arr) => {
+  let strForecast = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    strForecast += `...${arr[i]}\u00B0C in ${i + 1} days`;
+  }
+  return strForecast;
 };
