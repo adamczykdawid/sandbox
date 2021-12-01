c2664d3c250be0c85b7777e1cdb3cce87b807c94
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-01 13:52:32 +0100

    started working on DOM - some basic changes on elements

--- Colt-js/js-colt.js
+++ Colt-js/js-colt.js
@@ -1,1 +1,20 @@
+// console.log(document.querySelector("h1").innerText);
+// console.log(document.querySelector("ul").textContent);
+// console.log(document.querySelector("form").innerHTML);
+// console.log(document.querySelector("input").placeholder);
+// console.log(document.querySelectorAll("input"));
+// console.log(document.querySelector("img").src);
+// console.log(document.querySelector("a").href);
+// console.log(document.querySelector("form").nextSibling);
 
+///////////////////////changing alements
+const allLis = document.querySelectorAll("li");
+
+// for (let i = 0; i < allLis.length; i++) {
+//   console.log(allLis[i].innerText);
+//   allLis[i].innerText = "WE ARE THE CHAMPIONS!";
+// }
+
+for (let li of allLis) {
+  li.innerHTML = "WE ARE <b>THE CHAMPIONS</b>!";
+}
