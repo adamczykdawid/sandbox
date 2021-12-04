1d1ee80cbd5a7e0df992876c02dd669b582a33d7
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-04 23:25:50 +0100

    working on DOM

---
+++ Jonas/05-Guess-My-Number/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.querySelector('.message').textContent;
+document.querySelector('.message').textContent = 'Correct number!';
+
+document.querySelector('.number') = 13;
+document.querySelector('.score') = 20;
