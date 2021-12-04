5c836c22ac8bda9508614e74fd55f2f960db9190
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-05 00:12:50 +0100

    working on DOM

--- Jonas/05-Guess-My-Number/starter/script.js
+++ Jonas/05-Guess-My-Number/starter/script.js
@@ -1,7 +1,16 @@
 'use strict';
 
-document.querySelector('.message').textContent;
-document.querySelector('.message').textContent = 'Correct number!';
+// document.querySelector('.message').textContent;
+// document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number') = 13;
-document.querySelector('.score') = 20;
+// document.querySelector('.number') = 13;
+// document.querySelector('.score') = 10;
+
+// document.querySelector('.guess').value;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
