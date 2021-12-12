00b74a6da654d2f987b674138e3eff69089df296
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2021-12-13 00:18:11 +0100

    completed strings challanges

--- Jonas/09-Data-Structures-Operator/script.js
+++ Jonas/09-Data-Structures-Operator/script.js
@@ -49,28 +49,27 @@
 // };
 
 // /*
-// ///////////////////////////////////////
-// // String Methods Practice
+///////////////////////////////////////
+// String Methods Practice
 
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// // 🔴 Delayed Departure from FAO to TXL (11h25)
-// //              Arrival from BRU to FAO (11h45)
-// //   🔴 Delayed Arrival from HEL to FAO (12h05)
-// //            Departure from FAO to LIS (12h30)
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
 
-// const getCode = str => str.slice(0, 3).toUpperCase();
+const getCode = str => str.slice(0, 3).toUpperCase();
 
-// for (const flight of flights.split('+')) {
-//   const [type, from, to, time] = flight.split(';');
-//   const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
-//     '_',
-//     ' '
-//   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
-//   console.log(output);
-// }
-// */
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
 
 // ///////////////////////////////////////
 // // Coding Challenge #4
@@ -109,14 +108,26 @@ document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value.toLowerCase();
-  const textWorking = text.trim().split('_');
-  console.log(textWorking);
-  const [firstWord, secondWord] = textWorking;
-  const finalSecond = secondWord[0].toUpperCase() + secondWord.slice(1);
-  const finalText = firstWord + finalSecond;
+  // const text = document.querySelector('textarea').value.toLowerCase();
+  // const textWorking = text.trim().split('_');
 
-  console.log(finalText);
+  // const [firstWord, secondWord] = textWorking;
+  // const finalSecond = secondWord[0].toUpperCase() + secondWord.slice(1);
+  // const finalText = firstWord + finalSecond;
+
+  // console.log(finalText);
+  const text = document.querySelector('textarea').value.toLowerCase();
+  const rows = text.split('\n');
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'*'.repeat(i + 1)}`);
+  }
 });
 
 // /*
