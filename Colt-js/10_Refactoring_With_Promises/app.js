78385168be3c760941b618acf964d0f8c6bcec0c
Dawid Adamczyk <dawid_adamczyk@outlook.com>
2022-01-09 16:44:39 +0100

    axios

--- Colt-js/10_Refactoring_With_Promises/app.js
+++ Colt-js/10_Refactoring_With_Promises/app.js
@@ -119,8 +119,23 @@
 // // );
 
 //quick training:
-const prom = fetch("https://swapi.dev/api/planets/")
-  .then((res) => {
-    console.log("it worked");
-  })
-  .catch((err) => console.log("it didnt wotk"));
+const fetchNextPlanets = (url = "https://swapi.dev/api/planets/") => {
+  return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+  for (let planet of data.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(data.next);
+};
+
+fetchNextPlanets()
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .catch((err) => {
+    console.log("error", err);
+  });
